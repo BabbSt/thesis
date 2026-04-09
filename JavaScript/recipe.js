573e6209fa80d3c441c5b344467bea51d19b3ddb
BabbSt <step980@gmail.com>
2026-04-09 08:20:42 -0600

    Styling & add scroll to nav
    Handled overflow in side nav and added styling to match figma mockups including adding spine to all books and creating book cover preview background.

--- JavaScript/recipe.js
+++ JavaScript/recipe.js
@@ -1,10 +1,26 @@
 const nav = document.getElementById("bookNav");
 const toggleButtons = document.querySelectorAll(".toggleNav");
+const details = document.querySelectorAll('details');
+const background=document.getElementById('navBackdrop');
+
+let scrollLocked = false;
 
 toggleButtons.forEach((button)=>{
     button.addEventListener("click",()=>{
         console.log("clicked");
         console.log(nav.classList);
         nav.classList.toggle("open");
+        background.classList.toggle("hidden");
+        if(scrollLocked){
+            document.body.style.overflowY = '';
+            details.forEach(detail=>{
+                detail.open = false;
+            })
+        }else{
+            document.body.style.overflowY = 'clip';
+        }
+        scrollLocked=!scrollLocked;
     })
 })
+
+//add js to close details when nave is closed
